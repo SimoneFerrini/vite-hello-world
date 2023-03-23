62d1ce7b281df5063cf466a21a0028d02c5fc107
SimoneFerrini <ferrini.simone4@gmail.com>
2023-03-23 16:25:40 +0100

    ended with bonus track

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -4,4 +4,8 @@
     <img src="https://image.api.playstation.com/vulcan/img/rnd/202011/0714/Cu9fyu6DM41JPekXLf1neF9r.png" alt="">
 </template>
 
-<style></style>
+<style>
+    img{
+        width: 500px;
+    }
+</style>
